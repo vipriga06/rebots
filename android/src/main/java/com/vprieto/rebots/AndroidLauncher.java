b651fdad78d330a3cec87f21977315f43777900e
Victor Prieto <victorpg182006@gmail.com>
2026-05-06 23:56:02 +0200

    fix

---
+++ android/src/main/java/com/vprieto/rebots/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.vprieto.rebots;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.Joc;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        initialize(new Joc(), config);
+    }
+}
